c9fb7acbcaa97988c396ee53801425a3711eff24
2021pietcssachin148@poornima.org <2021pietcssachin148@poornima.org>
2023-09-07 10:06:15 +0530

    third commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import About from './About';
+// import About from './About';
 import Contact from './Contact';
 import Home from './Home';
 import Signup from './Signup';
@@ -16,7 +16,7 @@ export default function Header() {
   return (
     <BrowserRouter>
    <Routes>
-   <Route path='/about' element={<About />}>  </Route>
+   {/* <Route path='/about' element={<About />}>  </Route> */}
    <Route path='/' element={<Home/>}>  </Route>
    <Route path='/contact' element={<Contact />}>  </Route>
    <Route path='/signup' element={<Signup />}>  </Route>
